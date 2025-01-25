2c8f22418542b5ec52d4322a69fbaadbdbefbf3e
Srikanth Srungarapu <srikanth235@gmail.com>
2025-01-25 15:17:09 +0530

    fix: cosmetic fixes

--- components/icons.tsx
+++ components/icons.tsx
@@ -1120,7 +1120,12 @@ export function DotsIcon({ size = 16, ...props }: { size?: number }) {
   );
 }
 
-export function ExternalLinkIcon({ size = 16, ...props }: { size?: number }) {
+interface IconProps {
+  size?: number;
+  className?: string;
+}
+
+export function ExternalLinkIcon({ size = 16, className, ...props }: IconProps) {
   return (
     <svg
       width={size}
@@ -1131,6 +1136,7 @@ export function ExternalLinkIcon({ size = 16, ...props }: { size?: number }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      className={className}
       {...props}
     >
       <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
@@ -1141,5 +1147,15 @@ export function ExternalLinkIcon({ size = 16, ...props }: { size?: number }) {
 }
 
 export const HeartIcon = ({ size = 16}: { size?: number }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width=  {size} height={size} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg"
+  width=  {size} height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+  </svg>
 );
